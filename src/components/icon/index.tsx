18f7b132dbaf60d4b999bf54640a0114035c8ad3
Huhui <wishgood@gmail.com>
2020-11-20 19:50:06 +0800

    调整样式

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -5,8 +5,9 @@ export interface IconProps {
   name: string;
   size?: 'small' | 'medium' | 'large';
   color?: string;
+  className?: string;
 }
 
-export const Icon: FC<IconProps> = ({ name, size = 'small', color = '#8c8c8c' }) => {
-  return <i className={classNames('tbox-icon', name, `tbox-${size}`)} style={{ color }}/>
+export const Icon: FC<IconProps> = ({ name, size = 'small', color, className }) => {
+  return <i className={classNames('tbox-icon', name, `tbox-${size}`, className)} style={{ color }}/>
 }

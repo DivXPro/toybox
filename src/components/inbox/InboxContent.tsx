44c340db26cd576034fbf24582db6c78c1b12bbf
Huhui <wishgood@gmail.com>
2020-12-08 22:50:02 +0800

    调整

--- src/components/inbox/InboxContent.tsx
+++ src/components/inbox/InboxContent.tsx
@@ -1,15 +1,16 @@
-import React, { FC, useMemo, useCallback, useRef, useState, useEffect } from 'react';
+import React, { FC, useMemo, useCallback, useRef, useEffect } from 'react';
 import { Empty } from 'antd';
-import { useSize } from 'ahooks';
-import { FixedSizeList as List } from 'react-window';
-import InfiniteLoader from 'react-window-infinite-loader';
+import { useSize, useScroll, useThrottleFn } from 'ahooks';
+// import { FixedSizeList as List } from 'react-window';
+// import InfiniteLoader from 'react-window-infinite-loader';
 import { Notification, NotificationMessage } from './Notification';
 import { LoadingWrapper } from '../utils';
+// import Item from 'antd/lib/list/Item';
 
 
-const WIDTH = 360;
-const HEIGHT = 400;
-const ITEM_HEIGHT = 124;
+// const WIDTH = 360;
+// const HEIGHT = 400;
+// const ITEM_HEIGHT = 124;
 
 export interface InboxContentProps {
   onPick: (message?: NotificationMessage) => void;
@@ -25,25 +26,32 @@ export interface InboxContentProps {
 export const InboxContent: FC<InboxContentProps> = ({ loading, hasMore, onPick, messages = [], selectedId, loadMore, read, remove }) => {
   const ref = useRef<any>();
   const size = useSize(ref);
-  const isItemLoaded = useCallback((index: number) => !hasMore || index < messages.length, [hasMore, messages.length]);
-  const itemCount = useMemo(() => hasMore ? messages.length + 1 : messages.length, [hasMore, messages.length]);
+  const scroll = useScroll(ref);
+  // const isItemLoaded = useCallback((index: number) => !hasMore || index < messages.length, [hasMore, messages.length]);
+  // const itemCount = useMemo(() => hasMore ? messages.length + 1 : messages.length, [hasMore, messages.length]);
 
-  const Item = useCallback(({index, style} : { index: number, style: any}) => {
-    if (isItemLoaded(index)) {
-      return (
-        <Notification
-          key={index}
-          style={style}
-          onPick={onPick}
-          selected={messages[index].id === selectedId}
-          message={messages[index]}
-          remove={remove}
-          read={read}
-        />
-      )
-    }
-    return <div style={style}></div>
-  }, [isItemLoaded, onPick, messages, selectedId, remove, read])
+  const isTouchButton = useMemo(() => {
+    return scroll.top >= (size.height || 0);
+  }, [scroll.top, size.height]);
+
+  
+  // const Item = useCallback(({index, style} : { index: number, style: any}) => {
+  //   if (isItemLoaded(index)) {
+  //     return (
+  //       <Notification
+  //         key={index}
+  //         style={style}
+  //         onPick={onPick}
+  //         selected={messages[index].id === selectedId}
+  //         message={messages[index]}
+  //         remove={remove}
+  //         read={read}
+  //       />
+  //     )
+  //   }
+  //   return <div style={style}></div>
+  // }, [isItemLoaded, onPick, messages, selectedId, remove, read])
+
 
   const loadMoreItems = useCallback((start: number, stop: number) => {
     if (!loading) {
@@ -52,6 +60,17 @@ export const InboxContent: FC<InboxContentProps> = ({ loading, hasMore, onPick, 
     return null;
   }, [loadMore, loading]);
 
+  const { run } = useThrottleFn(
+    (start: number, stop: number) => loadMoreItems(start, stop),
+    { wait: 500 }
+  );
+
+  useEffect(() => {
+    if (isTouchButton && hasMore) {
+      run(messages.length, 10);
+    }
+  }, [isTouchButton, messages.length, hasMore, run]);
+
   const isEmpty = useMemo(() => {
     return (messages == null || messages.length === 0) && !loading
   }, [loading, messages]);
@@ -59,8 +78,21 @@ export const InboxContent: FC<InboxContentProps> = ({ loading, hasMore, onPick, 
   const messageList = useMemo(() => {
     return (
       <LoadingWrapper loading={messages == null}>
-        <div style={isEmpty ? {display: 'none'} : undefined}>
-          <InfiniteLoader isItemLoaded={isItemLoaded} itemCount={itemCount} loadMoreItems={loadMoreItems}>
+        <div className="notification-list" style={isEmpty ? {display: 'none'} : undefined} ref={ref}>
+          {
+            messages.map((message, index) => (
+              <Notification
+                key={index}
+                onPick={onPick}
+                selected={message.id === selectedId}
+                message={message}
+                remove={remove}
+                read={read}
+              />
+            ))
+          }
+
+          {/* <InfiniteLoader isItemLoaded={isItemLoaded} itemCount={itemCount} loadMoreItems={loadMoreItems}>
             {
               ({ onItemsRendered, ref }) => (
                 <List
@@ -75,15 +107,15 @@ export const InboxContent: FC<InboxContentProps> = ({ loading, hasMore, onPick, 
                 </List>
               )
             }
-          </InfiniteLoader>
+          </InfiniteLoader> */}
         </div>
         {isEmpty ? <div style={{ paddingTop: '60px', }}><Empty description="没发现消息通知" /></div> : null}
       </LoadingWrapper>
     );
-  }, [messages, isEmpty, isItemLoaded, itemCount, loadMoreItems, size.height, size.width, Item]);
+  }, [messages, isEmpty, onPick, selectedId, remove, read]);
 
   return (
-    <div className="tbox-inbox-content" ref={ref} >
+    <div className="tbox-inbox-content">
       {messageList}
     </div>
   );

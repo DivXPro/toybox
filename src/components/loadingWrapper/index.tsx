c140fb7ff29ac0c143353c87941ed087ddb0c1f8
Huhui <wishgood@gmail.com>
2020-12-15 16:58:37 +0800

    完善

--- src/components/loadingWrapper/index.tsx
+++ src/components/loadingWrapper/index.tsx
@@ -1,10 +1,22 @@
 import React, { FC } from 'react';
 import { Spin } from 'antd';
 
-export const LoadingWrapper: FC = () => {
+export interface LoadingWrapperProps {
+  size?: 'small' | 'default' | 'large';
+  loading: boolean;
+}
+
+export const LoadingWrapper: FC<LoadingWrapperProps> = ({ size, loading, children }) => {
   return (
-    <div className="loading-wrapper">
-      <Spin />
-    </div>
+    <React.Fragment>
+      {
+        loading
+          ?
+          <div className="loading-wrapper">
+            <Spin size={size} />
+          </div>
+          : children
+      }
+    </React.Fragment>
   );
 }

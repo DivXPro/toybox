0cea7e2bee711b29400b07e32c95e1e1357b4861
Huhui <wishgood@gmail.com>
2021-01-08 11:11:07 +0800

    fix

--- doczrc.js
+++
@@ -1,8 +0,0 @@
-export default {
-  ignore: ['README.md', 'changelog.md', 'contributing.md', 'LICENSE.md'],
-  base: './docs',
-  port: 8000,
-  hashRouter: true,
-  typescript: true,
-  files: '**/*.{md,markdown,mdx}'
-}
--- src/components/index.ts
+++ src/components/index.ts
@@ -9,7 +9,7 @@ export { default as Fields } from './Fields';
 export { default as Header } from './Header';
 export { default as ImpInput } from './ImpInput';
 export { default as Inbox } from './Inbox';
-export { default as IndexPage } from './IndexPage';
+export { default as IndexPage, SearchFindParam } from './IndexPage';
 export { default as ListPage } from './ListPage';
 export { default as LoadingWrapper } from './LoadingWrapper';
 export { default as MetaDescriptons } from './MetaDescriptions';

ab20a0fa983dd774664f90d9b274578ff2b36c5c
Huhui <wishgood@gmail.com>
2021-01-13 17:26:44 +0800

    fix: RemoteSelect

--- src/components/IndexPage/components/RemoteSelect.tsx
+++ src/components/IndexPage/components/RemoteSelect.tsx
@@ -1,8 +1,10 @@
-import React, { FC, useCallback, useState } from 'react';
+import React, { useRef, useEffect, useImperativeHandle, ForwardRefRenderFunction } from 'react';
 import { Select } from 'antd';
 import { OptionItem } from './IndexSearch';
+import useFetchOptions from '../../Fields/Select/hooks/useFetchOptions';
 
 export interface RemoteSelectProps {
+  value?: SelectValue;
   placeholder?: string;
   remote: (query: string) => Promise<OptionItem[]>;
   onChange?: (value: string | number) => void;
@@ -10,12 +12,36 @@ export interface RemoteSelectProps {
   allowClear?: boolean;
 }
 
-export const RemoteSelect: FC<RemoteSelectProps> = ({ placeholder, style, allowClear, remote, onChange }) => {
-  const [options, setOptions] = useState<OptionItem[]>();
-  const handleSearch = useCallback((query: string) => {
-    remote(query).then((data) => {
-      setOptions(data);
-    });
-  }, [remote]);
-  return <Select style={style} placeholder={placeholder} options={options} onSearch={handleSearch} onChange={onChange} allowClear={allowClear} />
+type SelectValue = React.ReactText | React.ReactText[];
+
+
+const RemoteSelect: ForwardRefRenderFunction<any, RemoteSelectProps> = ({ value, placeholder, style, allowClear, remote, onChange }, ref) => {
+  const inputRef = useRef<any>();
+  const [loading, remoteOptions, fetchData] = useFetchOptions(remote);
+  useEffect(() => {
+    const init = async () => {
+      await fetchData('');
+    };
+    init();
+  }, [fetchData]);
+
+  useImperativeHandle(ref, () => ({
+    ...(inputRef.current || {}),
+    fetchData,
+  }));
+
+  return <Select
+    ref={inputRef}
+    value={value}
+    style={style}
+    loading={loading}
+    placeholder={placeholder}
+    options={remoteOptions}
+    onSearch={fetchData}
+    onChange={onChange}
+    allowClear={allowClear}
+    showSearch
+  />
 }
+
+export default React.forwardRef(RemoteSelect);
